b9357f1edeecf343ae4c481f484f542e57892341
Sathvik Saran Atchukolu <94753192+sathviksaran@users.noreply.github.com>
2026-01-01 04:23:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -552,13 +552,13 @@ def logout():
     return redirect(url_for('login'))
 
 def get_response(intents_list, intents_json):
-    tag = intents_list[0]['intent']
-    list_of_intents = intents_json['intents']
-    for i in list_of_intents:
-        if i['tag'] == tag:
-            result = random.choice(i['responses'])
-            break
-    return result
+    tag = intents_list[0]['intent']
+    list_of_intents = intents_json['intents']
+    for i in list_of_intents:
+        if i['tag'] == tag:
+            result = random.choice(i['responses'])
+            break
+    return result
 
 if __name__ == "__main__":
   # Replace with your actual secret key
